d49ee383f807712abfa6df7829a687182afd2256
William Mao <willmao1@live.unc.edu>
2023-04-16 22:53:28 -0400

    fixed modal issue and added categories

--- src/pages/CardPage.js
+++ src/pages/CardPage.js
@@ -5,7 +5,7 @@ import Modal from 'react-bootstrap/Modal';
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import cog from "../images/settings-cog.svg"
-import TipModal from "../components/TipModal";
+import TipModal from "../components/CardModal";
 function CardPage({micromoment, handleShow}) {
 
   return (
--- src/pages/SearchPage.js
+++ src/pages/SearchPage.js
@@ -7,17 +7,16 @@ import Col from "react-bootstrap/Col";
 import MicromomentPreviewCard from "../components/MicromomentPreviewCard";
 import SearchBar from "../components/SearchBar";
 import MICROMOMENT_DATA from "../micromoment-data.json";
-import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button"
-import Modal from 'react-bootstrap/Modal';
-import TipModal from "../components/TipModal";
+import CardModal from "../components/CardModal";
 import cog from "../images/settings-cog.svg";
 
 function SearchPage({handlerFunc}) {
   const [searchQuery, setSearchQuery] = useState("");
-  const [show, setShow] = useState(false);
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  const [show, setShow] = useState(null);
+  const handleClose = () => setShow(null);
+  const handleShow = (e, id) => {
+    setShow(id);
+  }
     
   const onSearchChange = (e) => setSearchQuery(e.target.value);
   return (
@@ -35,46 +34,12 @@ function SearchPage({handlerFunc}) {
           micromoment.title.toLowerCase().includes(searchQuery.toLowerCase())
         ).map((micromoment) => (
           <>
-          <Row onClick={handleShow}>
+          <Row onClick={(e) => handleShow(e,micromoment.id)}>
             <Col>
               <MicromomentPreviewCard   micromoment={micromoment} />
             </Col>
           </Row>
-          <Modal show={show} onHide={handleClose}>
-          <Container fluid className="mt-3">
-          <Row>
-              <Col>
-                <img src={cog} width="35" />
-              </Col>
-          </Row>
-          <Row>
-              <Col>
-                <Card className="m-2 overlay-gradient">
-                  <Card.Body className = "overlay-content ">
-                    <Card.Title className = "fs-1" >{micromoment.title}</Card.Title>
-                    <Card.Subtitle className="mb-2 text-muted fs-4">Description:</Card.Subtitle>
-                    <Card.Text className="mb-3">
-                      {micromoment.description}
-                    </Card.Text>
-                    <Card.Subtitle className="mb-2 text-muted fs-4">Example:</Card.Subtitle>
-                    <Card.Text className="mb-1">
-                      {micromoment.example}
-                    </Card.Text>
-                  </Card.Body>
-                  <Card.Header className="text-center">Header</Card.Header>
-                </Card>
-              </Col>
-          </Row>
-          <Row>
-              <Col>
-                <TipModal tip = {micromoment.tip}/>
-              </Col>
-              <Col>
-                <Button onClick = {handleClose} className="float-right">Done</Button>
-              </Col>
-          </Row>
-          </Container>
-        </Modal>
+          <CardModal id = {micromoment.id} handleClose = {handleClose} show = {show} micromoment = {micromoment}/>
         </>
         ))}
       </Container>
